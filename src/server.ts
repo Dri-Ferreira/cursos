e079558ab95bd2e3445899cb09dfc3a77992160d
Adriano hilario <adrianoferreira31@gmail.com>
2022-12-08 16:52:35 -0300

    chamando a pasta public e adminjs no servidor

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
+import { adminjs, adminJsRouter } from './adminjs';
 import express from 'express';
 
 const app = express();
+
+app.use(express.static('public'));
+app.use(adminjs.options.rootPath, adminJsRouter);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
